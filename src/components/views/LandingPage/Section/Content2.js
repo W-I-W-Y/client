0bda544313596e27faff73e45023e22d80d6e1f5
지현 <spy03128@naver.com>
2021-10-13 08:51:31 +0900

    시도별 확진확잔 그래프 하는중

--- src/components/views/LandingPage/Section/Content2.js
+++ src/components/views/LandingPage/Section/Content2.js
@@ -1,7 +1,116 @@
-import React from "react";
+import React, { useState, useEffect, PureComponent } from "react";
 import { Row, Col } from "antd";
-
+import Axios from "axios";
+import { USER_SERVER } from "../../../Config";
+import { PieChart, Pie, Sector, ResponsiveContainer } from "recharts";
 function Content2() {
+  const demoUrl =
+    "https://codesandbox.io/s/pie-chart-with-customized-active-shape-y93si";
+  const [coronaGubun, setCoronaGubun] = useState([]);
+
+  useEffect(() => {
+    const headers = {
+      Authorization: `Bearer ` + localStorage.getItem("token"),
+    };
+
+    Axios.get(`${USER_SERVER}/api/corona`, { headers }).then(
+      (response, index) => {
+        if (response.data !== null) {
+          console.log("data check");
+          console.log(response.data);
+
+          response.data.coronaGubunDTOS.forEach((lists) => {
+            setCoronaGubun((state) => [
+              ...state,
+              {
+                gubun: lists.gubun,
+                defCnt: lists.defCnt,
+                incDec: lists.incDec,
+              },
+            ]);
+          });
+        } else {
+          alert("코로나정보를 가져오는데 실패했습니다.");
+        }
+      }
+    );
+  }, []);
+
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const {
+      cx,
+      cy,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      percent,
+      value,
+    } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
+
+    return (
+      <g>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path
+          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+          stroke={fill}
+          fill="none"
+        />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          textAnchor={textAnchor}
+          fill="#333"
+        >{`${value}명`}</text>
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          dy={18}
+          textAnchor={textAnchor}
+          fill="#999"
+        >
+          {`(Rate ${(percent * 100).toFixed(2)}%)`}
+        </text>
+      </g>
+    );
+  };
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onPieEnter = (_, index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <section
       className="content-section"
@@ -301,7 +410,27 @@ function Content2() {
             border: "none",
             position: "relative",
           }}
-        ></Col>
+        >
+          {/* <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={800} height={800}>
+              <text x="50%" y="50%" dy={8} textAnchor="middle">
+                xdd
+              </text>
+              <Pie
+                activeIndex={activeIndex}
+                activeShape={renderActiveShape}
+                data={coronaGubun}
+                cx="50%"
+                cy="50%"
+                innerRadius={60}
+                outerRadius={80}
+                fill=""
+                dataKey="incDec"
+                onMouseEnter={onPieEnter}
+              />
+            </PieChart>
+          </ResponsiveContainer> */}
+        </Col>
       </Row>
     </section>
   );
