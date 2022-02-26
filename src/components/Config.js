bf85c56396372d9463d8a22694f039159ca4547f
지현 <spy03128@naver.com>
2022-02-26 17:42:43 +0900

    Change config

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,6 +1,6 @@
 //SERVER ROUTES
 // export const USER_SERVER = "http://49.174.147.189:8080";
 
-export const USER_SERVER = "http://203.255.73.170:8080";
+// export const USER_SERVER = "http://203.255.73.170:8080";
 
-// export const USER_SERVER = "http://localhost:8080";
+export const USER_SERVER = "http://localhost:8080";
